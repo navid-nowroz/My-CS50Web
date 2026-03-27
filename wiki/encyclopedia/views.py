bfe4bd9a6fcb3abef026e5b8183a8e56ddf80728
Navid <navidnowroz.s@gmail.com>
2026-03-27 19:42:41 +0600

    wiki/ bug fixed

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,9 +2,24 @@ from django.shortcuts import render
 
 from . import util
 
+import markdown2
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def title(request, title):
+    content = util.get_entry(title)
+
+    if content == None:
+        return render(request, "encyclopedia/error_load.html", {
+            "title" : title
+        })    
+    
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "content": markdown2.markdown(content), "title" : title
+        })
 
