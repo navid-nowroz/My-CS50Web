eac625b6777b1a9d51fe6ef619045ea0cd86a9d0
Navid <navidnowroz.s@gmail.com>
2026-03-27 22:35:10 +0600

    fixed search results bug html not implemented.

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("search", views.search, name="search-result"),
     path("wiki/<str:title>", views.title, name="show-entry")
 ]
- 
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -23,3 +23,22 @@ def title(request, title):
             "content": markdown2.markdown(content), "title" : title
         })
 
+def search(request,):
+    query = request.GET.get("q")
+    entries = util.list_entries()
+    results = [entry for entry in entries if query.lower() in entry.lower()]
+
+    if query in entries:
+        return render(request, "encyclopedia/entry.html", {
+            "title" : query
+        })
+    
+    elif not len(results) > 0:
+        return render(request, "encyclopedia/error_load.html", {
+            "title" : query,
+        })
+    
+    else:
+        return render(request, "encyclopedia/search_results.html", {
+            "results" : results, "query": query
+        })
